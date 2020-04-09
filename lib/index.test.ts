534fbdaf14fd31baaf2cd8f29a0d636bc414f21e
nimoc <nimocc@126.com>
2020-04-09 15:56:48 +0800

    add now

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,23 +1,109 @@
 import * as otime from "./index"
+import dayjs from "dayjs"
 
 test("parseChina", function () {
     {
         const t = otime.parseChina({
-            layout: otime.second,
+            layout: otime.SecondLayout,
             date: "1992-12-19 00:00:00"
         })
-        expect(t.format(otime.second+ " ZZ")).toBe("1992-12-19 00:00:00 +0800")
-        expect(t.format(otime.second)).toBe("1992-12-19 00:00:00")
-        expect(t.format(otime.RFC3339)).toBe("1992-12-19T00:00:00+08:00")
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 00:00:00 +0800")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 00:00:00")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+08:00")
     }
     {
         const t = otime.parseChina({
-            layout: otime.second,
+            layout: otime.SecondLayout,
             date: "1992-12-19 11:22:33"
         })
-        expect(t.format(otime.second+ " ZZ")).toBe("1992-12-19 11:22:33 +0800")
-        expect(t.format(otime.second)).toBe("1992-12-19 11:22:33")
-        expect(t.format(otime.RFC3339)).toBe("1992-12-19T11:22:33+08:00")
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 11:22:33 +0800")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 11:22:33")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T11:22:33+08:00")
     }
+})
+test("parseJapan", function () {
+    {
+        const t = otime.parseJapan({
+            layout: otime.SecondLayout,
+            date: "1992-12-19 00:00:00"
+        })
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 00:00:00 +0900")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 00:00:00")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+09:00")
+    }
+    {
+        const t = otime.parseJapan({
+            layout: otime.SecondLayout,
+            date: "1992-12-19 11:22:33"
+        })
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 11:22:33 +0900")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 11:22:33")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T11:22:33+09:00")
+    }
+})
 
+test("toChina", function () {
+    {
+        const t = otime.parseJapan({
+            layout: otime.SecondLayout,
+            date: "1992-12-19 00:00:00"
+        })
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 00:00:00 +0900")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 00:00:00")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+09:00")
+        const chinaTime = t.toChina()
+        expect(chinaTime.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-18 23:00:00 +0800")
+        expect(chinaTime.format(otime.SecondLayout)).toBe("1992-12-18 23:00:00")
+        expect(chinaTime.format(otime.RFC3339Layout)).toBe("1992-12-18T23:00:00+08:00")
+    }
+})
+test("toJapan", function () {
+    {
+        const t = otime.parseChina({
+            layout: otime.SecondLayout,
+            date: "1992-12-19 00:00:00"
+        })
+        expect(t.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 00:00:00 +0800")
+        expect(t.format(otime.SecondLayout)).toBe("1992-12-19 00:00:00")
+        expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+08:00")
+        const japanTime = t.toJapan()
+        expect(japanTime.format(otime.SecondLayout+ " ZZ")).toBe("1992-12-19 01:00:00 +0900")
+        expect(japanTime.format(otime.SecondLayout)).toBe("1992-12-19 01:00:00")
+        expect(japanTime.format(otime.RFC3339Layout)).toBe("1992-12-19T01:00:00+09:00")
+    }
+})
+
+
+test("nowChina", function () {
+    let now = dayjs()
+    // @ts-ignore
+    now = now.utcOffset(8*60)
+    const dateString = [now.format("YYYY-MM-DDTHH:mm:ss"),'+08:00'].join("")
+    expect(otime.nowChina().format(otime.RFC3339Layout)).toBe(dateString)
+})
+test("nowJapan", function () {
+    let now = dayjs()
+    // @ts-ignore
+    now = now.utcOffset(9*60)
+    const dateString = [now.format("YYYY-MM-DDTHH:mm:ss"),'+09:00'].join("")
+    expect(otime.nowJapan().format(otime.RFC3339Layout)).toBe(dateString)
+})
+
+test("RFC3339", function () {
+    const t = otime.parseChina({
+        layout: otime.SecondLayout,
+        date: "1992-12-19 00:00:00"
+    })
+    expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+08:00")
+    expect(t.toJapan().format(otime.RFC3339Layout)).toBe("1992-12-19T01:00:00+09:00")
+})
+
+test("add", function () {
+    const t = otime.parseChina({
+        layout: otime.SecondLayout,
+        date: "1992-12-19 00:00:00"
+    })
+    expect(t.format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:00+08:00")
+
+    expect(t.add(otime.Second).format(otime.RFC3339Layout)).toBe("1992-12-19T00:00:01+08:00")
 })
--- lib/index.ts
+++ lib/index.ts
@@ -5,15 +5,50 @@ const dayjsUTC = require('dayjs/plugin/utc')
 
 dayjs.extend(customParseFormat)
 dayjs.extend(dayjsUTC)
+
+export const Millisecond = 1
+export const Second = Millisecond * 1000
+export const Minute = Second * 60
+export const Hour = Minute * 60
+
 class Time {
     _core: dayjs.Dayjs
-    constructor() {
-        this._core = dayjs()
+    constructor(dayValue: dayjs.Dayjs) {
+        this._core = dayValue
+    }
+    add(duration :number) :Time {
+        let time = this.clone()
+        time._core = time._core.add(duration, "millisecond")
+        return time
+    }
+    addDate(years:number, months:number, days:number) :Time {
+        const time = this.clone()
+        time._core = time._core.add(years, "year")
+            .add(months, "month")
+            .add(days, "month")
+        return time
     }
     format(layout:string) :string {
         return this._core.format(layout)
     }
+    clone():Time {
+        const day = this._core.clone()
+        return new Time(day)
+    }
+    utcOffset(offset:utcOffset):Time {
+        const time = this.clone()
+        // @ts-ignore
+        time._core = time._core.utcOffset(offset._minute)
+        return time
+    }
+    toChina() :Time {
+        return this.utcOffset(UtcOffset(8))
+    }
+    toJapan() :Time {
+        return this.utcOffset(UtcOffset(9))
+    }
 }
+
 interface iParseData {
     layout:string
     date:string
@@ -36,37 +71,56 @@ function minteToUTCZone(minute:number) :string {
     }
     return symbol + hourValue + minuteValue
 }
+// please use otime.UtcOffset(hour) create utcOffset
+interface utcOffset {
+    _minute:number
+    _note: string
+}
+export function UtcOffset (hour:number) :utcOffset {
+    return {
+        _minute: hour*60,
+        _note: "",
+    }
+}
 // 时区列表 https://zh.wikipedia.org/wiki/%E6%97%B6%E5%8C%BA%E5%88%97%E8%A1%A8
-export function parseUTCOffsetMinute(data:iParseData, minute:number) :Time {
-    const time = new Time()
+export function parseUTCOffset(data:iParseData, utcOffset: utcOffset) :Time {
+    const time = new Time(dayjs())
     if (data.date == "") {
         throw new Error("@og/time parseChina(data) data.date can not be empty string")
     }
-
-    time._core = dayjs(data.date + minteToUTCZone(minute), {
+    time._core = dayjs(data.date + minteToUTCZone(utcOffset._minute), {
         format: data.layout+"ZZ",
         utc: true,
     })
     // @ts-ignore
-    time._core = time._core.utcOffset(minute)
+    time._core = time._core.utcOffset(utcOffset._minute)
     return time
 }
 // 时区列表 https://zh.wikipedia.org/wiki/%E6%97%B6%E5%8C%BA%E5%88%97%E8%A1%A8
-export function parseUTCOffsetHour(data:iParseData, hour:number): Time {
-    return parseUTCOffsetMinute(data, hour*60)
+export function parseUTC(data:iParseData, offsetHour:number): Time {
+    return parseUTCOffset(data, UtcOffset(offsetHour))
 }
 export function parseChina(data:iParseData): Time {
-    data = Object.assign({}, data)
-    return parseUTCOffsetHour(data, 8)
+    return parseUTC(data, 8)
 }
 export function parseJapan(data:iParseData): Time {
     data = Object.assign({}, data)
-    return parseUTCOffsetHour(data, 9)
+    return parseUTC(data, 9)
 }
-export const second = "YYYY-MM-DD HH:mm:ss"
-export const Minute = "YYYY-MM-DD HH:mm"
-export const hour = "YYYY-MM-DD HH"
-export const day = "YYYY-MM-DD"
-export const month = "YYYY-MM"
-export const year = "YYYY"
-export const RFC3339 = "YYYY-MM-DDTHH:mm:ssZ"
+export function nowUTC() :Time {
+    return new Time(dayjs()).utcOffset(UtcOffset(0))
+}
+export function nowChina() :Time {
+    return new Time(dayjs()).utcOffset(UtcOffset(8))
+}
+export function nowJapan() :Time {
+    return new Time(dayjs()).utcOffset(UtcOffset(9))
+}
+export const MillisecondLayout = "YYYY-MM-DD HH:mm:ss.SSS"
+export const SecondLayout = "YYYY-MM-DD HH:mm:ss"
+export const MinuteLayout = "YYYY-MM-DD HH:mm"
+export const HourLayout = "YYYY-MM-DD HH"
+export const DayLayout = "YYYY-MM-DD"
+export const MonthLayout = "YYYY-MM"
+export const YearLayout = "YYYY"
+export const RFC3339Layout = "YYYY-MM-DDTHH:mm:ssZ"
